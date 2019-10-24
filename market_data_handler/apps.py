eed141ab1aa605c7220bfc4feb928cdc3f246e84
marcusmello <mandealista@gmail.com>
2019-10-24 19:33:23 -0300

    Reshape modules' namespacing

--- market_data_handler/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MarketDataHandlerConfig(AppConfig):
-    name = 'market_data_handler'
--- strategy_components/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StrategyComponentsConfig(AppConfig):
-    name = 'strategy_components'
