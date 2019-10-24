80e8f3f8ba06c7e28d3464420ced84dfa3f014a6
marcusmello <mandealista@gmail.com>
2019-10-24 15:56:57 -0300

    Updated to clearer syntax

---
+++ trader/models.py
@@ -0,0 +1,117 @@
+# Create your models here.
+#from django.db import models
+
+import importlib
+import time
+
+from components.models import MetaData
+from market_data_handler.models import RealMarketData, BacktestMarketData
+from order_handler.models import BacktestOrder
+
+# TODO : Atenção para os métodos privados
+
+'''ORGANIZAÇÃO DOS DADOS
+    
+    - metadata      -- Parâmetros de controle e acompanhamento do trade, como:
+                        
+                        - id do metadata
+                        - id do setup de estratégia
+                        - dados da corretora (usuário, senha)
+                        - identificação do mercado (nome da corretora e ativo)
+                        - trade_interval
+                        - candle_interval
+                        - posição:
+                            * lado
+                            * tamanho
+                            * ref_stop_price
+'''
+
+class Trader:
+
+    def __init__(self, metadata_name: str):
+
+        # TODO : Metadata como model do django
+        self.metadata = MetaData(id=metadata_name)
+
+        Strategy = getattr(importlib.import_module("components.models"),
+                           self.metadata.strategy_name)
+
+        # TODO : Strategy como model do django
+        self.strategy = Strategy(id=self.metadata.setup_id)
+
+    def run(self, mode):
+        """
+
+        :type mode: list
+        """
+        real_trade = 'rt' in mode
+        backtesting = 'bt' in mode
+        both = bool(real_trade and backtesting)
+
+        if real_trade:
+            # Corre em celery run_for_real
+            pass
+
+        if backtesting:
+            # Corre em celery run_in_back_testing
+            pass
+
+        if both:
+            # Corre cada um em um celery diferente
+            pass
+
+    # @celery
+    def run_for_real(self):
+        '''Todos os passos do trade, como verificar stop, definir posição, disparar ordem
+        corretamente para corretora correspondente, etc. Repetir indefinidamente
+
+        :return:
+        '''
+
+        # TODO : verificar se o trader está ativo para o mesmo trade. Emitir alerta caso esteja.
+
+        data_handler = RealMarketData(self.metadata)
+
+        order = getattr(importlib.import_module("order_handler.models"),
+                                  self.metadata.exchange_name)
+
+        while True:
+
+            self.strategy.data_to_analyze = data_handler.refresh_data()
+
+            signal = self.strategy.chek_for_signal(self.metadata.position)
+
+            if signal.is_trade:
+
+                trade = order.execute(signal.trade)
+
+                if trade.is_done:
+
+                    self.metadata.position.side = signal.trade["new_side"]
+                    # TODO : Appendar no trade_history
+
+            #strategy.update_ref_prices
+
+            #TODO : função do timesleep
+            time.sleep(self.strategy.calcule_sleep_for((self.metadata.trade_interval)))
+
+    # @celery
+    def run_in_back_testing(self,
+                            from_sample="Oldest",
+                            trade_history='Append',
+                            continuous_mode=True):
+        '''Todos os passos do trade, como verificar stop, definir posição, disparar ordem
+        simulada, appendar o histórico de trade em banco de dados próprio, etc.
+
+
+        :keyword from_sample(str)   -- Timestamp of the market data sample that will starts
+                                       the simulation. E.g.: '2019-10-15 13:04:00'
+                                       Default="Oldest", takes the oldest data available.
+
+        :keyword trade_history(str) -- 'Append' or 'Replace' if matching table is found.
+
+        :keyword
+
+        '''
+
+        pass
